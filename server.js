91500520ebbce9cc28cc9a6403e78b3fcb7fa919
Tboy-tolu <ogunbotutoluwanimi@gmail.com>
2025-08-20 13:13:44 +0100

    Update and rename server.js to keep-alive.js

--- server.js
+++
@@ -1,8 +0,0 @@
-
-// server.js - keep-alive web server
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => res.send("✅ Bot is running 24/7"));
-app.listen(PORT, () => console.log(`Keep-alive server running on port ${PORT}`));
